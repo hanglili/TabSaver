18bd832a8fd42aa0e8a4806be07ec5949c7373ef
Hang Li Li <hang1997li@gmail.com>
2019-05-19 15:56:48 +0100

    Implemented Basic save tabs and view tabs for one window.

--- client/popup.js
+++ client/popup.js
@@ -1,15 +1,19 @@
-let changeColor = document.getElementById('changeColor');
+let saveTabsButton = document.getElementById('saveTabs');
+let viewTabsButton = document.getElementById('viewTabs');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+saveTabsButton.onclick = function(element) {
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+        chrome.storage.sync.set({tabs: tabs}, function() {
+          console.log("Saved new tabs");
+        });
     });
+};
+
+viewTabsButton.onclick = function(element) {
+  chrome.storage.sync.get(['tabs'], function(result) {
+    console.log("Viewing saved tabs");
+    result.tabs.forEach(tab => {
+      console.log(tab.url);
+    });
+  });
 };
